c6909dbcd5256eebf203650681aae7357f76196d
Nur Hasan Masum <nurhasanmasum@gmail.com>
2025-06-28 13:56:38 +0000

    update firebase config based on // For Firebase JS SDK v7.20.0 and later

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/a
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDNFKX0y3B8M-HRqZNGjheMUgHWxX_c-6Y",
+  authDomain: "bridgescore-app.firebaseapp.com",
+  projectId: "bridgescore-app",
+  storageBucket: "bridgescore-app.appspot.com",
+  messagingSenderId: "1011165776209",
+  appId: "1:1011165776209:web:e82e723cfd377da659eb70",
+  measurementId: "G-3E242ZM778"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
