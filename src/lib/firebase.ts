93f39d3c784443c30b3d46bcebf91b2b6efcf5a6
Nur Hasan Masum <nurhasanmasum@gmail.com>
2025-06-29 05:09:03 +0000

    update my code base
    // Import the functions you need from the SDKs you

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,12 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyDNFKX0y3B8M-HRqZNGjheMUgHWxX_c-6Y",
   authDomain: "bridgescore-app.firebaseapp.com",
+  databaseURL: "https://bridgescore-app-default-rtdb.firebaseio.com",
   projectId: "bridgescore-app",
-  storageBucket: "bridgescore-app.appspot.com",
+  storageBucket: "bridgescore-app.firebasestorage.app",
   messagingSenderId: "1011165776209",
-  appId: "1:1011165776209:web:e82e723cfd377da659eb70",
-  measurementId: "G-3E242ZM778"
+  appId: "1:1011165776209:web:a3d46c6f0f5c4bca59eb70",
+  measurementId: "G-D42JPN6307"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
